68b2f29a7256278ee9d17a6066cef552892af285
dhaven <david.haven.dh@gmail.com>
2017-01-20 17:48:29 +0100

    client basic design + view pictures + upload

--- desktop-app/kivytestUI.py
+++ desktop-app/kivytestUI.py
@@ -4,39 +4,97 @@ from kivy.uix.boxlayout import BoxLayout
 kivy.require('1.9.1') # replace with your current kivy version !
 
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+from kivy.uix.popup import Popup
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
 from kivy.uix.button import Button
-from kivy.uix.scrollview import ScrollView
-from kivy.core.window import Window
+
+from boto3 import client
+import os
+import requests
+import threading
 
 
-class LoginScreen(GridLayout):
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class ButBrown(Button):
+    pass
+
+class DownDialog(RelativeLayout):
+    textFileName = StringProperty(None)
+    download = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class Root(BoxLayout):
+    loadfile = ObjectProperty(None)
+    layout_content = ObjectProperty(None)
+    list_of_images = []
 
     def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        super(Root, self).__init__(**kwargs)
+        Clock.schedule_once(self.post_init, 1)
 
-class MyApp(App):
+    def post_init(self, *args):
+        assert self.layout_content is not None
+        self.layout_content.bind(minimum_height=self.layout_content.setter('height'))
 
-    def build(self):
-        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-        # Make sure the height is such that there is something to scroll.
-        layout.bind(minimum_height=layout.setter('height'))
-        for i in range(100):
-            btn = Button(text=str(i), size_hint_y=None, height=40)
-            layout.add_widget(btn)
-        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height), scroll_type=['bars','content'])
-        root.add_widget(layout)
-        return root
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def refresh(self):
+        conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
+        for key in conn.list_objects(Bucket='memoirebuckettest')['Contents']:
+            if key['Key'] not in self.list_of_images:
+                btn = ButBrown(text=key['Key'],size_hint_y=None)
+                btn.bind(on_release=self.show_down)
+                self.list_of_images.append(key['Key'])
+                self.layout_content.add_widget(btn)
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_down(self,*args):
+        print args[0].text
+        content = DownDialog(textFileName=args[0].text,cancel=self.dismiss_popup)
+        self._popup = Popup(title="Download file",
+                            content=content,
+                            size_hint=(0.5,0.5))
+        self._popup.open()
+
+
+    def load(self, path, filename):
+        self.image = {'file':open(os.path.join(path, filename[0]),'rb')}
+        #self.text_input.text = stream.read()
+        threading.Thread(target=self.uploadPicture).start()
+
+        self.dismiss_popup()
+
+    def uploadPicture(self):
+        url = "http://127.0.0.1:8000/polls/"
+        s = requests.session()
+        r = s.get(url)
+        headers = {'X-CSRFToken': s.cookies["csrftoken"]}
+        r = s.post(url, files=self.image, cookies=s.cookies, headers=headers)
+        self.image['file'].close()
+
+
+
+class Editor(App):
+    pass
+
+Factory.register('LoadDialog', cls=LoadDialog)
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    Editor().run()
